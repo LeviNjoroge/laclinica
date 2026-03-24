54c03ce78040d9f33f914fb2e97e0fc8fb87b4be
LeviNjoroge <levynjoroge1@gmail.com>
2026-03-24 20:06:03 +0300

    finalizing work on the backend part

--- php/index.php
+++ php/index.php
@@ -1,39 +1,3 @@
-<?php
-session_start();
-require("./backend/config/database.php");
-
-if (isset($_POST["submit"])) {
-    $first_name = filter_input(INPUT_POST,'first_name',FILTER_SANITIZE_SPECIAL_CHARS);
-    $middle_name = filter_input(INPUT_POST,'middle_name',FILTER_SANITIZE_SPECIAL_CHARS);
-    $last_name = filter_input(INPUT_POST,'last_name',FILTER_SANITIZE_SPECIAL_CHARS);
-    $gender = $_POST['gender'];
-    $date_of_birth = $_POST['date_of_birth'];
-    $patient_number = filter_input(INPUT_POST,'patient_number',FILTER_SANITIZE_NUMBER_INT);
-    $reg_date = $_POST['registration_date'];
-
-    // check if patient already exists
-    $check_patient = mysqli_query($conn, "SELECT * FROM PATIENTS_REGISTRATION WHERE patient_number = '$patient_number'");
-    if (mysqli_num_rows($check_patient) > 0) {
-        echo '<script> alert("Patient already registered!") </script>';
-    } else {
-        $query_insert_data_into_patients_reg_table = "INSERT INTO PATIENTS_REGISTRATION VALUES('$patient_number', '$first_name','$middle_name','$last_name', '$gender', '$date_of_birth', '$reg_date')";
-        $insert_data_into_patients_reg_table = mysqli_query($conn, $query_insert_data_into_patients_reg_table);
-        echo '<script> alert("Patient registered successfully!") </script>';
-    }
-    $_SESSION["first_name"] = $first_name;
-    $_SESSION["middle_name"] = $middle_name;
-    $_SESSION["last_name"] = $last_name;
-    $_SESSION["patient_number"] = $patient_number;
-
-    header("Location: ./vitals_form.php");
-    exit;
-}
-
-if (isset($_POST["cancel"])) {
-    header("Location: ./");
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -47,10 +11,10 @@ if (isset($_POST["cancel"])) {
 <body>
     <div class="container">
         <h2>Patient Registration Form</h2>
-        <form method="POST">
+        <form method="POST" action="./backend/actions/save_patients.php">
             <div class="first_name">
                 <label for="first_name">First Name:</label>
-                <input name="first_name" id="first_name" placeholder="Joe" required>
+                <input name="first_name" type="text" id="first_name" placeholder="Joe" required>
             </div>
 
             <div class="middle_name">
@@ -65,7 +29,7 @@ if (isset($_POST["cancel"])) {
 
             <div class="gender">
                 <label for="gender">Gender:</label>
-                <input type="radio" name="gender" id="Male" value="Male"> Male
+                <input type="radio" name="gender" id="Male" value="Male" required> Male
                 <input type="radio" name="gender" id="Female" value="Female"> Female
             </div>
 
