9740c6460afceb963b6aa4ae4a8b4377a96b8b92
LeviNjoroge <levynjoroge1@gmail.com>
2026-03-24 17:55:46 +0300

    still working on the backend part

--- php/patients_listing.php
+++ php/patients_listing.php
@@ -1,7 +1,35 @@
 <?php
 require("./backend/config/database.php");
 
-// required details: Patients_name, Age, BMI Status []
+// required details: Patients_name, Age, BMI Status [Underweight, Normal, Overweight], Last Assessment Date
+$patients_listing_query = "SELECT FIRST_NAME, MIDDLE_NAME, LAST_NAME, DATE_OF_BIRTH, BMI, VISIT_DATE
+                            FROM PATIENTS_REGISTRATION P
+                            JOIN VITALS_RECORDS V
+                            ON P.PATIENT_NUMBER = V.PATIENT_NUMBER
+                            AND V.VISIT_DATE = (SELECT MAX(V2.VISIT_DATE)
+			                                    FROM VITALS_RECORDS V2
+			                                    WHERE V2.PATIENT_NUMBER = P.PATIENT_NUMBER)
+";
+$patients_listing = mysqli_query($conn, $patients_listing_query);
+
+$current_date = date("Y-m-d");
+
+function BMI_Comment($BMI){
+    if  ($BMI<18.5){
+        return 'Underweight';
+    }elseif ($BMI<25) {
+        return 'Normal';
+    }else{
+        return 'Overweight';
+    }
+}
+
+function calculate_AGE($date_of_birth){
+    $dob = new DateTime($date_of_birth);
+    $today = new DateTime();
+    $age = $today->diff($dob)->y;
+    return $age;
+}
 ?>
 
 <!DOCTYPE html>
@@ -22,9 +50,9 @@ require("./backend/config/database.php");
             <input type="button" value="reset">
         </div>
 
-        <table border="1">
+        <table>
             <thead>
-                <tr>
+                <tr class="table_head">
                     <th>Patient Name</th>
                     <th>Age</th>
                     <th>BMI Status</th>
@@ -33,12 +61,24 @@ require("./backend/config/database.php");
             </thead>
             <tbody>
                 <!-- add table data here-->
-                <tr>
-                    <td>N/A</td>
-                    <td>N/A</td>
-                    <td>N/A</td>
-                    <td>N/A</td>
-                </tr>
+                    <?php
+                        if(mysqli_num_rows($patients_listing) > 0){
+                            while($patient = mysqli_fetch_assoc($patients_listing)){
+                                echo "<tr>";
+                                echo "<td>".$patient['FIRST_NAME']." ".$patient['MIDDLE_NAME']." ".$patient['LAST_NAME']."</td>";
+                                echo "<td>".calculate_AGE($patient["DATE_OF_BIRTH"])."</td>";
+                                echo "<td>".BMI_Comment($patient['BMI'])."</td>";
+                                echo "<td>".$patient['VISIT_DATE']."</td>";
+                                echo "</tr>";
+                            }
+                        } else {
+                        echo "<tr>";
+                            for($i = 0; $i< 4; $i++){
+                                echo "<td> N/A </td>";
+                            }
+                        echo "<tr>";
+                        }
+                    ?>
             </tbody>
         </table>
     </div>
